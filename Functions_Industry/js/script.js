0e94aa6cbfc7abb1cbc10f19e46b52711afe1e36
Jessica Brandt <SchoolWork@Jessicas-MacBook-Pro.local>
2013-05-31 00:21:14 -0600

    week4_corrections

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,8 +3,7 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
 
-var days = Number (prompt("How many days have you been in the unit?", 0));
-var rate = Number (prompt("How much do you pay a month for your unit?", 0));
+
 
 function proratedBill (days, rate){
 	var dailyCost = rate / 30;
@@ -12,4 +11,4 @@ function proratedBill (days, rate){
 	console.log(bill)
 }
 
-proratedBill()
+proratedBill(prompt("How many days have you been in the unit?", 0), prompt("How much do you pay a month for your unit?", 0))
