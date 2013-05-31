b47d80f1ecc91deb031931995558d86f5215b609
Jessica Brandt <SchoolWork@Jessicas-MacBook-Pro.local>
2013-05-31 00:17:00 -0600

    week 4_ corrections

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,16 +3,12 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
 
-//var min = Number (prompt("How many minutes do you read at a time?"));
-//var day = Number (prompt("How many days do you read a week?"));
-
 var timeReading = function(min,day){
 	var totalTime = min * day;
-	return (totalTime);
-	//(totalTime >= 140) ? "Keep up the good work!" : "Try getting more reading in."
+	console.log(totalTime);
+	var a = (totalTime >= 150) ? "Keep up the good work!" : "Try getting more reading in.";
+	console.log(a);
 }
 
-timeReading = (totalTime >= 140) ? "Keep up the good work!" : "Try getting more reading in."
-var a = timeReading(Number(prompt("How many minutes do you spend reading?")), Number(prompt("How many days do you read a week?")));
-//timeReading = (totalTime >= 140) ? "Keep up the good work!" : "Try getting more reading in."
-console.log (a);
+
+timeReading (prompt("How many minutes do you spend reading?"), prompt("How many days do you read a week?"));
